0c213d4c8ed340c1868d8db6f29cfec7a4a17f48
Xayah-Hina <ycxiang14sep@gmail.com>
2022-08-10 18:05:41 +0800

    dev point neighbor searcher

---
+++ src/physics/lib/basic.h
@@ -0,0 +1,8 @@
+#ifndef HINAPE_BASIC_H
+#define HINAPE_BASIC_H
+
+#include "Eigen/Eigen"
+
+using Vector3D = Eigen::Vector3f;
+
+#endif //HINAPE_BASIC_H
--- src/physics/physics_objects/particle_system_data3.h
+++ src/physics/physics_objects/particle_system_data3.h
@@ -1,10 +1,20 @@
 #ifndef HINAPE_PARTICLE_SYSTEM_DATA3_H
 #define HINAPE_PARTICLE_SYSTEM_DATA3_H
 
+#include "../lib/basic.h"
+#include "../lib/array1.h"
+#include "../util/point_neighbor_searcher3.h"
+
+#include <memory>
+#include <vector>
+
 namespace HinaPE
 {
 class ParticleSystemData3
 {
+    using ScalarData = Array1<double>;
+    using VectorData = Array1<Vector3D>;
+
 private:
     double _radius;
     double _mass;
@@ -13,12 +23,18 @@ private:
     size_t _velocity_idx;
     size_t _force_idx;
 
+    std::vector<ScalarData> _scalar_data_list;
+    std::vector<VectorData> _vector_data_list;
+    PointNeighborSearcher3Ptr _neighbor_searcher;
+    std::vector<std::vector<size_t>> _neighborLists;
+
 public:
     ParticleSystemData3();
     explicit ParticleSystemData3(size_t number_of_particles);
-    ParticleSystemData3(const ParticleSystemData3& other);
+    ParticleSystemData3(const ParticleSystemData3 &other);
     virtual ~ParticleSystemData3();
 };
+using ParticleSystemData3Ptr = std::shared_ptr<ParticleSystemData3>;
 }
 
 #endif //HINAPE_PARTICLE_SYSTEM_DATA3_H
