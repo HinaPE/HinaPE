52a68d979184771ee8edd0d41a15e7ff52a4a626
Xayah-Hina <ycxiang14sep@gmail.com>
2022-08-10 17:04:32 +0800

    dev particle system data

--- src/physics/physics_objects/particle_system_data3.cpp
+++ src/physics/physics_objects/particle_system_data3.cpp
@@ -1,1 +1,8 @@
 #include "particle_system_data3.h"
+HinaPE::ParticleSystemData3::ParticleSystemData3() = default;
+HinaPE::ParticleSystemData3::ParticleSystemData3(size_t number_of_particles)
+{
+
+}
+HinaPE::ParticleSystemData3::ParticleSystemData3(const HinaPE::ParticleSystemData3 &other) = default;
+HinaPE::ParticleSystemData3::~ParticleSystemData3() = default;
--- src/physics/physics_objects/particle_system_data3.h
+++ src/physics/physics_objects/particle_system_data3.h
@@ -5,7 +5,19 @@ namespace HinaPE
 {
 class ParticleSystemData3
 {
+private:
+    double _radius;
+    double _mass;
+    size_t _number_of_particles;
+    size_t _position_idx;
+    size_t _velocity_idx;
+    size_t _force_idx;
 
+public:
+    ParticleSystemData3();
+    explicit ParticleSystemData3(size_t number_of_particles);
+    ParticleSystemData3(const ParticleSystemData3& other);
+    virtual ~ParticleSystemData3();
 };
 }
 
