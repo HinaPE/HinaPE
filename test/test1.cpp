7348409b474aafdde8f18f3d5a86fa3bce4af7a7
Xayah-Hina <ycxiang14sep@gmail.com>
2022-08-12 15:08:18 +0800

    remove test & fix ubuntu building

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,33 +1,5 @@
 #include "test1.h"
 
-#include "lib/mathlib.h"
-#include "vector"
-
-#include <thread>
-#include <iostream>
-
-#ifdef HINAPE_TBB_SUPPORT
-#include "tbb/parallel_for.h"
-#endif
-
 void HinaPE::TEST::Test1::test()
 {
-    std::atomic<int> counter = 0;
-    std::thread t1([&]()
-                   {
-                       for (int i = 0; i < 10000; i++)
-                       {
-                           counter++;
-                       }
-                   });
-    std::thread t2([&]()
-                   {
-                       for (int i = 0; i < 10000; i++)
-                       {
-                           counter++;
-                       }
-                   });
-    t1.join();
-    t2.join();
-    std::cout << counter << std::endl;
 }
