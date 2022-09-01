7363ed6e076eac46f23f3c5a9cb81783e55983c1
Xayah-Hina <ycxiang14sep@gmail.com>
2022-09-01 17:39:48 +0800

    subtle change

--- src/physics/physics_objects/particle_emitter3.h
+++ src/physics/physics_objects/particle_emitter3.h
@@ -1,10 +1,20 @@
 #ifndef HINAPE_PARTICLE_EMITTER3_H
 #define HINAPE_PARTICLE_EMITTER3_H
 
+#include "../common.h"
+
 #include "particle_system_data3.h"
+
+#include <random>
+#include <variant>
 
 namespace HinaPE
 {
+struct PointGenerator
+{
+    std::mt19937 rng;
+};
+
 class ParticleEmitter3
 {
 public:
@@ -24,7 +34,9 @@ private:
 
 private:
     ParticleSystemData3Ptr data_ptr;
+    std::variant<PointGenerator> particle_generator;
 };
+
 }
 
 #endif //HINAPE_PARTICLE_EMITTER3_H
