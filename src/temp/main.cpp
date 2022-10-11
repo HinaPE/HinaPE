fa90e4d19c0900e0e46263cca82327eef17055e9
Xayah-Hina <ycxiang14sep@gmail.com>
2022-10-11 15:14:40 +0800

    checkpoint - complete sph sys

--- src/temp/main.cpp
+++ src/temp/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "math_lib/bounding_box3.h"
+#include "math_lib/plane3.h"
+#include "math_lib/box3.h"
+#include "math_lib/rigid_body_collider3.h"
+#include "math_lib/sphere3.h"
+#include "math_lib/implicit_surface_set3.h"
 #include "sph/sph_solver3.h"
 #include "kernel/volume_particle_emitter3.h"
 using namespace jet;
 
-int main()
+auto main() -> int
 {
     BoundingBox3D domain(Vector3D(), Vector3D(1, 2, 1));
 
@@ -12,16 +17,24 @@ int main()
 
     solver->setPseudoViscosityCoefficient(0.0);
 
-    auto emitter = VolumeParticleEmitter3::builder()
-            //            .withImplicitSurface(surfaceSet)
-            //            .withSpacing(targetSpacing)
-            //            .withMaxRegion(sourceBound)
-            .withIsOneShot(true).makeShared();
+    BoundingBox3D sourceBound(domain);
+    sourceBound.expand(-0.02);
+
+    auto plane = Plane3::builder().withNormal({0, 1, 0}).withPoint({0, 0.25 * domain.height(), 0}).makeShared();
+
+    auto sphere = Sphere3::builder().withCenter(domain.midPoint()).withRadius(0.15 * domain.width()).makeShared();
+
+    auto surfaceSet = ImplicitSurfaceSet3::builder().withExplicitSurfaces({plane, sphere}).makeShared();
+
+    auto emitter = VolumeParticleEmitter3::builder().withImplicitSurface(surfaceSet).withSpacing(0.02).withMaxRegion(sourceBound).withIsOneShot(true).makeShared();
 
     solver->setEmitter(emitter);
 
-    BoundingBox3D sourceBound(domain);
-    sourceBound.expand(-0.02);
+    auto box = Box3::builder().withIsNormalFlipped(true).withBoundingBox(domain).makeShared();
+
+    auto collider = RigidBodyCollider3::builder().withSurface(box).makeShared();
+
+    solver->setCollider(collider);
 
     return 0;
 }
