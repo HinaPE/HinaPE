e09f00744653a991883ee31e723cdce19b05b010
Xayah-Hina <ycxiang14sep@gmail.com>
2022-10-21 16:30:16 +0800

    fix Windows build

--- HinaPE.cpp
+++ HinaPE.cpp
@@ -1,5 +1,12 @@
-class HinaPE
-{
-public:
+#include "render-engine/platform/platform.h"
+#include "render-engine/util/rand.h"
 
-};
+auto main(int argc, char **argv) -> int
+{
+    RNG::seed();
+    Launch_Settings set;
+    Platform plt;
+    App app(set, &plt);
+    plt.loop(app);
+    return 0;
+}
