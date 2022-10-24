d79d4d7a9e9e35c0b0c02049f63b88e3b49dacc0
Xayah-Hina <ycxiang14sep@gmail.com>
2022-10-24 18:04:28 +0800

    PBD solver Builder

--- cloth-engine/solver/pbd/pbd_solver.cpp
+++ cloth-engine/solver/pbd/pbd_solver.cpp
@@ -1,1 +1,6 @@
 #include "pbd_solver.h"
+
+auto HinaPE::ClothEngine::PBDSolver::builder() -> HinaPE::ClothEngine::PBDSolver::Builder { return {}; }
+auto HinaPE::ClothEngine::PBDSolver::Builder::build() const -> HinaPE::ClothEngine::PBDSolver { return {}; }
+auto HinaPE::ClothEngine::PBDSolver::Builder::makeShared() const -> HinaPE::ClothEngine::PBDSolverPtr { return {new PBDSolver(), [](PBDSolver *obj) { delete obj; }}; }
+void HinaPE::ClothEngine::PBDSolver::update(double dt) {}
