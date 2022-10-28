8953e32057d0dd25690551245ae9af6c747cce24
Xayah-Hina <ycxiang14sep@gmail.com>
2022-10-28 15:59:48 +0800

    Hina 1.1.1

--- physics-engine/cloth-engine/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "solver/pbd/pbd_solver.h"
-
-auto main() -> int
-{
-    auto solver = HinaPE::ClothEngine::PBDSolver::builder().with_stiffness(0).makeShared();
-
-    solver->print_hello();
-    return 0;
-}
