1ad1cd5e6a61120b25adf6d87bf7f7f1eafe4cf3
Xayah-Hina <ycxiang14sep@gmail.com>
2022-11-02 14:39:19 +0800

    update Textured Mesh

--- render-engine/platform/mesh.h
+++ render-engine/platform/mesh.h
@@ -5,22 +5,40 @@
 
 #include "../lib/mathlib.h"
 
+#include <vector>
+#include <iostream>
+#include <string>
+#include <memory>
+
 namespace HinaPE::RenderEngine
 {
 
 using Vector3 = Vec3;
 using Vector2 = Vec2;
 
-class TexturedMesh
+class Texture final
 {
 public:
-    TexturedMesh() = default; // Empty Constructor;
-    TexturedMesh(const TexturedMesh &src) = delete;
-    TexturedMesh(TexturedMesh &&src) noexcept;
-    ~TexturedMesh();
-    void operator=(const TexturedMesh &src) = delete;
-    auto operator=(TexturedMesh &&src) noexcept -> TexturedMesh &;
+    GLuint ID;
 
+public:
+    Texture(const std::string &path);
+    Texture(const Texture &texture) = delete;
+    Texture(Texture &&texture) noexcept = default;
+    ~Texture() = default;
+    auto operator=(const Texture &texture) -> Texture & = delete;
+    auto operator=(Texture &&texture) noexcept -> Texture & = default;
+
+public:
+    void bind(int texture_idx = 0) const;
+
+private:
+    int width, height, nr_channels;
+};
+using TexturePtr = std::shared_ptr<Texture>;
+
+class TexturedMesh final
+{
 public:
     using Index = GLuint;
     struct Vertex
@@ -30,7 +48,31 @@ public:
         Vector2 tex_coord;
         GLuint id;
     };
+
+public:
+    TexturedMesh(std::vector<Vertex> &&vertices, std::vector<Index> &&indices);
+    TexturedMesh(const TexturedMesh &src) = delete;
+    TexturedMesh(TexturedMesh &&src) noexcept = default;
+    ~TexturedMesh();
+    void operator=(const TexturedMesh &src) = delete;
+    auto operator=(TexturedMesh &&src) noexcept -> TexturedMesh & = default;
+
+public:
+    void render();
+    void update();
+    void attach_texture(const TexturePtr &texture);
+
+private:
+    bool dirty;
+    GLuint VAO, VBO, EBO;
+    GLuint n_elem;
+
+    std::vector<Vertex> _verts;
+    std::vector<Index> _idxs;
+    std::vector<TexturePtr> _textures;
+    BBox _bbox;
 };
+using TexturedMeshPtr = std::shared_ptr<TexturedMesh>;
 }
 
 #endif //HINAPE_MESH_H
