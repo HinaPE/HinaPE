2adbb290e1a37ab1ee62d174c4ab95df6e81103d
Xayah-Hina <ycxiang14sep@gmail.com>
2022-11-04 11:29:23 +0800

    rollback

--- render-engine/scene_new/renderer_workbench.cpp
+++
@@ -1,6 +0,0 @@
-#include "renderer_workbench.h"
-
-void HinaPE::RenderEngine::RendererWorkbench::render_mesh(const HinaPE::RenderEngine::TexturedMesh &mesh, const HinaPE::RenderEngine::RendererWorkbench::MeshOpt &opt)
-{
-
-}
