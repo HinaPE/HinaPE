4d282907d3ee12845f05733477030d61694939b7
Xayah-Hina <ycxiang14sep@gmail.com>
2022-11-04 12:03:55 +0800

    fix build issue

---
+++ render-engine/math_api.h
@@ -0,0 +1,20 @@
+#ifndef HINAPE_MATH_API_H
+#define HINAPE_MATH_API_H
+
+#include "math/vector2.h"
+#include "math/vector3.h"
+#include "math/vector4.h"
+#include "math/matrix3x3.h"
+#include "math/matrix4x4.h"
+#include "math/quaternion.h"
+#include "geometry/bounding_box3.h"
+
+namespace HinaPE::RenderEngine
+{
+using real = float;
+using mVector2 = Vector3F;
+using mVector3 = Vector3F;
+using mBBox = BoundingBox3F;
+}
+
+#endif //HINAPE_MATH_API_H
