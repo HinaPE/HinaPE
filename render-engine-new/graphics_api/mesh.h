7301527b7c3d93feeb33ae2e4ee680cc02d04fee
Xayah-Hina <ycxiang14sep@gmail.com>
2022-11-04 12:01:38 +0800

    dev OpenGL backend

---
+++ render-engine-new/graphics_api/mesh.h
@@ -0,0 +1,14 @@
+#ifndef HINAPE_RENDER_ENGINE_PRO_MESH_H
+#define HINAPE_RENDER_ENGINE_PRO_MESH_H
+
+#include "math_api.h"
+
+namespace HinaPE::RenderEngine
+{
+class TexturedMesh
+{
+
+};
+}
+
+#endif //HINAPE_RENDER_ENGINE_PRO_MESH_H
