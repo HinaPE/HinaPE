2bfc5c9e438ab11ea48a6bf4183277ccd3a08c70
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-12 13:35:55 +0800

    enable instancing

--- examples/pbd-cloth/api.cpp
+++ examples/pbd-cloth/api.cpp
@@ -38,17 +38,26 @@ void HinaPE::Cloth::Api::ui_sidebar()
 		std::map<std::string, std::vector<Kasumi::TexturePtr>> textures;
 		textures["diffuse"].push_back(std::make_shared<Kasumi::Texture>(std::string(MyAssetDir) + "TexturesCom_FabricWool0022_2_seamless_S.jpg"));
 		auto obj_id = _scene->add_object(std::make_shared<Kasumi::Model>(std::move(res_v), std::move(indices), std::move(textures)));
+		auto &model = _scene->get_object(obj_id)->get_model();
+		std::vector<Kasumi::Pose> poses;
+		Kasumi::Pose pose1;
+		pose1.position = {0, 0, 0};
+		poses.push_back(pose1);
+		Kasumi::Pose pose2;
+		pose2.position = {5, 5, 5};
+		poses.push_back(pose2);
+		model->setup_instancing(poses);
 		_solvers[_scene->get_object(obj_id)->get_model()] = solver;
 	}
 }
 void HinaPE::Cloth::Api::sync()
 {
-//	for (auto &pair: _solvers)
-//	{
-//		auto &vert_mesh = pair.first->vertices(0);
-//		const auto &vert_physics = pair.second->vertices();
-//
-//		for (int i = 0; i < vert_mesh.size(); ++i)
-//			vert_mesh[i].position = vert_physics[i];
-//	}
+	for (auto &pair: _solvers)
+	{
+		auto &vert_mesh = pair.first->vertices(0);
+		const auto &vert_physics = pair.second->vertices();
+
+		for (int i = 0; i < vert_mesh.size(); ++i)
+			vert_mesh[i].position = vert_physics[i];
+	}
 }
