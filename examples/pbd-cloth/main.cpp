96049e43dc5a45fad66c224e1a5281ab00c901c7
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-12 11:45:23 +0800

    smooth launch procedure

--- examples/pbd-cloth/main.cpp
+++ examples/pbd-cloth/main.cpp
@@ -3,9 +3,8 @@
 
 auto main() -> int
 {
-    auto platform = std::make_shared<Kasumi::Platform>(1920, 768);
-    auto app = std::make_shared<Kasumi::Renderer>("empty.txt");
-    app->load_api(std::make_shared<HinaPE::Cloth::Api>());
-    platform->launch(app);
-    return 0;
+	std::make_shared<Kasumi::Renderer>("empty.txt")
+			->load_api(std::make_shared<HinaPE::Cloth::Api>())
+			->launch();
+	return 0;
 }
