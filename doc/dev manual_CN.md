909fea9cd274e457f388ac7071b4124075cfca83
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-13 00:43:21 +0800

    2D painter example

--- doc/dev manual_CN.md
+++ doc/dev manual_CN.md
@@ -6,9 +6,9 @@
 
 - backends：通用渲染后端，所有与OpenGL（或其他图形API）相关的功能类全部被封封装在这里。
 - common：通用数学/几何库。一个大项目只需要依赖一个common就可以了。如果Kasumi渲染器外面还有工程包含它，那么这个common是不使用的，而是会使用外面那个common。（比如HinaPE便是）
-- renderer app：通用渲染器。这个层级与渲染后端（OpenGL等）无关，完全解耦，包含场景、UI等操作。
+- renderer app：通用渲染器。这个层级与渲染后端的实现（OpenGL等）无关，完全解耦，包含场景、UI等操作。
 
-### Backends：通用渲染后端开发与使用
+### Backends：通用渲染后端
 
 
 
---
+++ examples/painting2D/main.cpp
@@ -0,0 +1,73 @@
+#include "glad/glad.h"
+
+#include "backends/platform.h"
+#include "backends/shader.h"
+#include "backends/framebuffer.h"
+
+class Painter : public Kasumi::App
+{
+public:
+	Painter() : Kasumi::App(800, 600, "2D Painter") {}
+	void prepare() final
+	{
+		_shader = std::make_shared<Kasumi::Shader>(std::string(MyShaderDir) + "painter_vertex.glsl", std::string(MyShaderDir) + "painter_fragment.glsl");
+		_screen_shader = std::make_shared<Kasumi::Shader>(std::string(MyShaderDir) + "screen_vertex.glsl", std::string(MyShaderDir) + "screen_fragment.glsl");
+		_framebuffer = std::make_shared<Kasumi::Framebuffer>(_width, _height);
+
+		std::array<float, 24> screen = {
+				-1.0, -1.0, 0.0, 0.0,
+				-1.0, 1.0, 0.0, 1.0,
+				1.0, -1.0, 1.0, 0.0,
+
+				1.0, -1.0, 1.0, 0.0,
+				-1.0, 1.0, 0.0, 1.0,
+				1.0, 1.0, 1.0, 1.0
+		};
+		glGenVertexArrays(1, &_vao);
+		glBindVertexArray(_vao);
+
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+		glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(float), &screen[0], GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void *>(0));
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void *>(2 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		glBindVertexArray(0);
+	}
+	void update(double dt) final
+	{
+		_framebuffer->use();
+		_shader->use();
+		mVector2 screen(_width, _height);
+		static int time = 0;
+		_shader->uniform("iTime", time++);
+		time %= 60;
+		_shader->uniform("iResolution", screen);
+		glBindVertexArray(_vao);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		_framebuffer->unuse();
+		_screen_shader->use();
+		glBindVertexArray(_vao);
+		_framebuffer->bind_texture();
+		glLineWidth(3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+	}
+
+private:
+	Kasumi::ShaderPtr _shader;
+	Kasumi::ShaderPtr _screen_shader;
+	Kasumi::FramebufferPtr _framebuffer;
+
+	unsigned int _vao;
+};
+
+auto main() -> int
+{
+	std::make_shared<Painter>()->launch();
+	return 0;
+}
