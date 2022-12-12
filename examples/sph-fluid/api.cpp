ec32c14054620d9e6e9a3563ffe4f4cb0e0fca45
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-12 17:50:59 +0800

    fix sph fluid

--- examples/sph-fluid/api.cpp
+++ examples/sph-fluid/api.cpp
@@ -3,10 +3,27 @@
 
 void Api::step(float dt)
 {
+	if (_fluid_solver == nullptr)
+		return;
+
+	static HinaPE::Frame frame(0, 1.0 / 60.0);
+	_fluid_solver->update(frame);
+
+	sync();
 }
 
 void Api::ui_sidebar()
 {
+	if (!_inited)
+	{
+		HinaPE::Logging::mute();
+		_bounding_model = std::make_shared<Kasumi::Model>("cube", Color::NO_COLORS);
+		_bounding_model_scene = _scene->get_object(_scene->add_object(_bounding_model));
+		_bounding_model->_opt.render_surface = false; // dont render the surface
+		_bounding_model->_opt.render_bbox = true; // render the bounding box instead
+		_bounding_model->_opt.bbox_color = Color::GRAY;
+		_inited = true;
+	}
 	static int phase = 0;
 	switch (phase)
 	{
@@ -23,19 +40,45 @@ void Api::ui_sidebar()
 
 void Api::sync()
 {
+
+	auto const &positions = _fluid_solver->sphSystemData()->positions();
+
+	std::vector<Kasumi::Pose> poses;
+	for (auto const &pos: positions)
+	{
+		Kasumi::Pose pose;
+		pose.position = {pos.x, pos.y, pos.z};
+		pose.scale = {0.01, 0.01, 0.01};
+		poses.push_back(pose);
+	}
+	if (positions.size() > 0)
+	{
+		static bool first = true;
+		if (first)
+		{
+			_particle_model = std::make_shared<Kasumi::Model>("sphere", Color::BLUE);
+			_particle_model_scene = _scene->get_object(_scene->add_object(_particle_model));
+			_particle_model->setup_instancing(poses);
+			first = false;
+		} else
+		{
+			_particle_model->_opt.instance_count = poses.size();
+			_particle_model->_opt.instance_matrices.clear();
+			for (auto &pose: poses)
+				_particle_model->_opt.instance_matrices.emplace_back(pose.get_model_matrix().transposed());
+		}
+	}
 }
 
 auto Api::phase1() -> bool
 {
 	if (ImGui::CollapsingHeader("Create Fluid Domain"), ImGuiTreeNodeFlags_DefaultOpen)
 	{
-		static float R = 1.0f;
-		ImGui::SliderFloat("Size", &R, 0.01f, 10.0f, "%.2f");
+		ImGui::SliderFloat("Size", &_opt.bounding_box_size, 0.01f, 10.0f, "%.2f");
+		_bounding_model_scene->scale() = mVector3(_opt.bounding_box_size, _opt.bounding_box_size, _opt.bounding_box_size);
 		if (ImGui::Button("Add"))
 		{
-			_fluid_bounding_model = std::make_shared<Kasumi::Model>("cube", Color::BLACK);
-			auto scene_obj = _scene->get_object(_scene->add_object(_fluid_bounding_model));
-			scene_obj->scale() = mVector3(R, R, R);
+			_bounding_model->_opt.bbox_color = Color::BLACK;
 			return true;
 		}
 	}
@@ -45,9 +88,42 @@ auto Api::phase2() -> bool
 {
 	if (ImGui::CollapsingHeader("Create Fluid"), ImGuiTreeNodeFlags_DefaultOpen)
 	{
-		ImGui::Text("Hello");
+		ImGui::SliderFloat("Density", &_opt.target_density, 500.f, 2000.0f, "%10.f");
+		ImGui::SliderFloat("Spacing", &_opt.target_spacing, 0.01f, 1.0f, "%.02f");
 		if (ImGui::Button("Add"))
 		{
+			auto plane = HinaPE::Plane3::builder()
+					.withNormal({0, 1, 0})
+					.withPoint({0, 0.25 * _opt.bounding_box_size, 0})
+					.makeShared();
+			auto sphere = HinaPE::Sphere3::builder()
+					.withCenter({_bounding_model_scene->position().x, _bounding_model_scene->position().y, _bounding_model_scene->position().z})
+					.withRadius(0.15 * _opt.bounding_box_size)
+					.makeShared();
+			auto surfaceSet = HinaPE::ImplicitSurfaceSet3::builder()
+					.withExplicitSurfaces({plane, sphere})
+					.makeShared();
+			_fluid_emitter = HinaPE::Fluid::VolumeParticleEmitter3::builder()
+					.withImplicitSurface(surfaceSet).withSpacing(_opt.target_spacing)
+					.withMaxRegion(HinaPE::BoundingBox3D({-_opt.bounding_box_size, -_opt.bounding_box_size, -_opt.bounding_box_size}, {_opt.bounding_box_size, _opt.bounding_box_size, _opt.bounding_box_size}))
+					.withMaxNumberOfParticles(10000)
+					.withIsOneShot(false)
+					.makeShared();
+			auto box = HinaPE::Box3::builder()
+					.withIsNormalFlipped(true)
+					.withBoundingBox(HinaPE::BoundingBox3D({-_opt.bounding_box_size, -_opt.bounding_box_size, -_opt.bounding_box_size}, {_opt.bounding_box_size, _opt.bounding_box_size, _opt.bounding_box_size}))
+					.makeShared();
+			_fluid_collider = HinaPE::RigidBodyCollider3::builder()
+					.withSurface(box)
+					.makeShared();
+			_fluid_solver = HinaPE::Fluid::SphSolver3::builder()
+					.withTargetDensity(_opt.target_density)
+					.withTargetSpacing(_opt.target_spacing)
+					.makeShared();
+			_fluid_solver->setEmitter(_fluid_emitter);
+			_fluid_solver->setCollider(_fluid_collider);
+			_fluid_solver->setPseudoViscosityCoefficient(_opt.pseudo_viscosity_coefficient);
+
 			return true;
 		}
 	}
--- examples/sph-fluid/api.h
+++ examples/sph-fluid/api.h
@@ -3,7 +3,20 @@
 
 #include "renderer/api.h"
 #include "renderer/scene/scene.h"
+
+#include "common/geometry/bounding_box3.h"
+#include "common/geometry/plane3.h"
+#include "common/geometry/box3.h"
+#include "common/geometry/cylinder3.h"
+#include "common/geometry/rigid_body_collider3.h"
+#include "common/geometry/sphere3.h"
+#include "common/geometry/implicit_surface_set3.h"
+#include "common/logging.h"
+#include "common/array/array_utils-inl.h"
 #include "fluid/solver/particle/sph/sph_solver3.h"
+#include "fluid/solver/particle/sph/sph_solver3.h"
+#include "fluid/solver/particle/pcisph/pci_sph_solver3.h"
+#include "fluid/emitter/volume_particle_emitter3.h"
 
 class Api : public Kasumi::Api
 {
@@ -13,13 +26,12 @@ public:
 	void sync();
 
 private:
-	enum FluidType { SPH };
-	struct FluidOpt
+	struct SPHFluidOpt
 	{
+		float bounding_box_size = 1.f;
 		float target_density = 1000.f;
 		float target_spacing = 0.05f;
 
-		FluidType type = SPH;
 		// SPH Field
 		float pseudo_viscosity_coefficient = 0.f;
 	} _opt;
@@ -29,9 +41,18 @@ private:
 	auto phase2() -> bool;
 
 private:
+	// physics part
 	HinaPE::Fluid::SphSolver3Ptr _fluid_solver;
-	Kasumi::ModelPtr _fluid_bounding_model;
-	Kasumi::ModelPtr _fluid_particle_model;
+	HinaPE::Fluid::ParticleEmitter3Ptr _fluid_emitter;
+	HinaPE::RigidBodyCollider3Ptr _fluid_collider;
+
+	// scene part
+	Kasumi::ModelPtr _bounding_model;
+	Kasumi::ModelPtr _particle_model;
+	Kasumi::SceneObjectPtr _bounding_model_scene;
+	Kasumi::SceneObjectPtr _particle_model_scene;
+
+	bool _inited = false;
 };
 
 #endif //HINAPE_API_H
