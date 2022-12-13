b0ea344b70c624718e864878234b363311cd6e70
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-13 15:42:14 +0800

    update miku flag

--- examples/pbd-cloth/api.cpp
+++ examples/pbd-cloth/api.cpp
@@ -15,43 +15,52 @@ void Api::ui_sidebar()
 	static std::array<int, 2> dim2 = {30, 30};
 	static float stiffness = 1;
 
-	ImGui::Text("Cloth");
-	ImGui::SliderFloat2("width/height", size.data(), 1, 10);
-	ImGui::SliderInt2("rows/cols", dim2.data(), 1, 100);
-	ImGui::SliderFloat("stiffness", &stiffness, 0.f, 3.0f, "%.1f");
-
-	if (ImGui::Button("Create"))
+	static int phase = 0;
+	if (phase == 0)
 	{
-		// init solver data
-		HinaPE::Cloth::PBDSolver::Opt opt;
-		opt.rows = dim2[0];
-		opt.cols = dim2[1];
-		opt.width = size[0];
-		opt.height = size[1];
-		_cloth_solver = std::make_shared<HinaPE::Cloth::PBDSolver>(opt);
-		_cloth_solver->_opt.distance_constraint_stiffness = stiffness;
+		ImGui::Text("Cloth");
+		ImGui::SliderFloat2("width/height", size.data(), 1, 10);
+		ImGui::SliderInt2("rows/cols", dim2.data(), 1, 100);
+		ImGui::SliderFloat("stiffness", &stiffness, 0.f, 3.0f, "%.1f");
 
-		// init corresponding rendering model
-		auto &verts = _cloth_solver->vertices();
-		auto &indices = _cloth_solver->indices();
-		std::vector<Kasumi::Model::Vertex> res_v;
-		for (auto &v: verts)
+		if (ImGui::Button("Create"))
 		{
-			Kasumi::Model::Vertex v_;
-			v_.position = v;
-			v_.tex_coord = {0.5f - v.x / static_cast<float>(size[0]), 0.5f - v.y / static_cast<float>(size[1])};
-			res_v.emplace_back(v_);
+			// init solver data
+			HinaPE::Cloth::PBDSolver::Opt opt;
+			opt.rows = dim2[0];
+			opt.cols = dim2[1];
+			opt.width = size[0];
+			opt.height = size[1];
+			_cloth_solver = std::make_shared<HinaPE::Cloth::PBDSolver>(opt);
+			_cloth_solver->_opt.distance_constraint_stiffness = stiffness;
+
+			// init corresponding rendering model
+			auto &verts = _cloth_solver->vertices();
+			auto &indices = _cloth_solver->indices();
+			std::vector<Kasumi::Model::Vertex> res_v;
+			for (auto &v: verts)
+			{
+				Kasumi::Model::Vertex v_;
+				v_.position = v;
+				v_.tex_coord = {0.5f - v.x / static_cast<float>(size[0]), 0.5f - v.y / static_cast<float>(size[1])};
+				res_v.emplace_back(v_);
+			}
+			std::map<std::string, std::vector<Kasumi::TexturePtr>> textures;
+			textures["diffuse"].push_back(std::make_shared<Kasumi::Texture>(std::string(MyAssetDir) + "miku.jpg"));
+			_cloth_model = std::make_shared<Kasumi::Model>(std::move(res_v), std::move(indices), std::move(textures));
+			auto id1 = _scene->add_object(_cloth_model);
+			_scene->get_object(id1)->_name = "Miku Flag";
+
+			_scene->_state.selected_object_id = id1;
+
+			_cloth_particle_model = std::make_shared<Kasumi::Model>("sphere", Color::RED);
+			_cloth_particle_model->instancing();
+			auto id2 = _scene->add_object(_cloth_particle_model);
+			_scene->get_object(id2)->_name = "Particles Instance";
+
+			sync();
+			++phase;
 		}
-		std::map<std::string, std::vector<Kasumi::TexturePtr>> textures;
-		textures["diffuse"].push_back(std::make_shared<Kasumi::Texture>(std::string(MyAssetDir) + "miku.jpg"));
-		_cloth_model = std::make_shared<Kasumi::Model>(std::move(res_v), std::move(indices), std::move(textures));
-		_scene->_state.selected_object_id = _scene->add_object(_cloth_model);
-
-		_cloth_particle_model = std::make_shared<Kasumi::Model>("sphere", Color::RED);
-		_cloth_particle_model->instancing();
-		_scene->add_object(_cloth_particle_model);
-
-		sync();
 	}
 }
 void Api::sync() const
