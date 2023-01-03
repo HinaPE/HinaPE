e124459afbfd253da419c4510b351a66c5f6365e
Xayah-Hina <ycxiang14sep@gmail.com>
2023-01-03 16:34:53 +0800

    update testing emitter

--- examples/302-pcisph-fluid/api.cpp
+++ examples/302-pcisph-fluid/api.cpp
@@ -16,22 +16,43 @@ TestEmitter::TestEmitter()
 	_data = std::make_shared<HinaPE::Fluid::ParticleSystemData3>();
 	_emitter->setTarget(_data);
 }
-
-void TestEmitter::test_one_frame()
+#include <iostream>
+void TestEmitter::test_one_frame() const
 {
-	_emitter->update(time, 0.01f);
+	_emitter->update(_time, 0.01f);
+	std::cout << _data->positions().size() << std::endl;
 }
 
 void PCISPHExample::prepare()
 {
-	_tests.push_back(std::make_shared<TestEmitter>());
+	_test_emitter = std::make_shared<TestEmitter>();
+
+	// scene obj
+	auto model = std::make_shared<Kasumi::Model>("sphere", Color::RED);
+	model->instancing();
+	_fluid_obj = _scene->get_object(_scene->add_object(model));
 }
-void PCISPHExample::step(float dt) {}
 void PCISPHExample::ui_sidebar() {}
-void PCISPHExample::sync() {}
+void PCISPHExample::sync() const
+{
+	auto const &positions = _test_emitter->_data->positions();
+	std::vector<Kasumi::Pose> poses;
+	for (auto const &pos: positions)
+	{
+		Kasumi::Pose pose;
+		pose.position = {pos.x, pos.y, pos.z};
+		pose.scale = {0.01, 0.01, 0.01};
+		poses.push_back(pose);
+	}
+	_fluid_obj->get_model()->clear_instances();
+	if (!poses.empty())
+		_fluid_obj->get_model()->add_instances(poses);
+}
 void PCISPHExample::key(int key, int scancode, int action, int mods)
 {
 	if (key == GLFW_KEY_N)
-		for (auto const &test: _tests)
-			test->test_one_frame();
+	{
+		_test_emitter->test_one_frame();
+		sync();
+	}
 }
--- examples/302-pcisph-fluid/api.h
+++ examples/302-pcisph-fluid/api.h
@@ -6,31 +6,30 @@
 #include "geometry/sphere3.h"
 #include "emitter/volume_particle_emitter3.h"
 
-class Test { public: virtual void test_one_frame() = 0; };
-using TestPtr = std::shared_ptr<Test>;
-
-class TestEmitter : public Test
+class TestEmitter
 {
 public:
 	TestEmitter();
-	void test_one_frame() final;
+	void test_one_frame() const;
 
-private:
 	HinaPE::Fluid::ParticleEmitter3Ptr _emitter;
 	HinaPE::Fluid::ParticleSystemData3Ptr _data;
-	double time{0.0};
+	double _time{0.0};
 };
+using TestEmitterPtr = std::shared_ptr<TestEmitter>;
 
 class PCISPHExample : public Kasumi::Api
 {
 public:
 	void prepare() final;
-	void step(float dt) final;
 	void ui_sidebar() final;
-	void key(int key, int scancode, int action, int mods) override;
-	void sync();
+	void key(int key, int scancode, int action, int mods) final;
+	void sync() const;
 
-	std::vector<TestPtr> _tests;
+	TestEmitterPtr _test_emitter;
+
+	// scene part
+	Kasumi::SceneObjectPtr _fluid_obj;
 };
 
 #endif //HINAPE_API_H
