b1bc2da1429d1f7f38d8d5a19ca8f901db342a23
Xayah-Hina <ycxiang14sep@gmail.com>
2023-01-31 20:51:10 +0800

    r: revert changes

--- physics/_experimental/src/base/parallel.h
+++ physics/_experimental/src/base/parallel.h
@@ -4,7 +4,7 @@
 namespace Hina::Base
 {
 enum class ExecutionPolicy { Serial, Parallel };
-/*
+
 template<typename RandomIterator, typename T>
 void parallelFill(const RandomIterator &begin, const RandomIterator &end, const T &value, ExecutionPolicy policy = ExecutionPolicy::Parallel);
 
@@ -38,92 +38,8 @@ void parallelSort(RandomIterator begin, RandomIterator end, CompareFunction comp
 void set_max_number_of_threads(unsigned int num_threads);
 
 auto max_number_of_threads() -> unsigned int;
-*/
 }
-
 #ifdef HINAPE_TBB
-#include <tbb/parallel_for.h>
-//#include <tbb/parallel_reduce.h>
-#include <tbb/parallel_sort.h>
-#include <tbb/task.h>
-
-namespace Hina::Base
-{
-template<typename IndexType, typename Function>
-void parallelFor(IndexType begin_index, IndexType end_index, const Function &function, ExecutionPolicy policy = ExecutionPolicy::Parallel)
-{
-	if (begin_index > end_index)
-		return;
-
-	if (policy == ExecutionPolicy::Parallel)
-		tbb::parallel_for(begin_index, end_index, function);
-	else
-		for (auto i = begin_index; i < end_index; ++i)
-			function(i);
-}
-
-template<typename IndexType, typename Function>
-void parallelRangeFor(IndexType begin_index, IndexType end_index, const Function &function, ExecutionPolicy policy = ExecutionPolicy::Parallel)
-{
-	if (begin_index > end_index)
-		return;
-
-	if (policy == ExecutionPolicy::Parallel)
-		tbb::parallel_for(tbb::blocked_range<IndexType>(begin_index, end_index),
-						  [&function](const tbb::blocked_range<IndexType> &range)
-						  {
-							  function(range.begin(), range.end());
-						  });
-	else
-		func(begin_index, end_index);
-}
-
-template<typename IndexType, typename Function>
-void parallelFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, const Function &function, ExecutionPolicy policy = ExecutionPolicy::Parallel)
-{
-	parallelFor(begin_indexY, end_indexY,
-				[&](IndexType j)
-				{
-					for (IndexType i = begin_indexX; i < end_indexX; ++i)
-						function(i, j);
-				},
-				policy);
-}
-
-template<typename IndexType, typename Function>
-void parallelRangeFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, const Function &function, ExecutionPolicy policy = ExecutionPolicy::Parallel)
-{
-	parallelRangeFor(begin_indexY, end_indexY,
-					 [&](IndexType jBegin, IndexType jEnd)
-					 {
-						 function(begin_indexX, end_indexX, jBegin, jEnd);
-					 },
-					 policy);
-}
-
-template<typename IndexType, typename Function>
-void parallelFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, IndexType begin_indexZ, IndexType end_indexZ, const Function &function, ExecutionPolicy policy = ExecutionPolicy::Parallel)
-{
-	parallelFor(begin_indexZ, end_indexZ,
-				[&](IndexType k)
-				{
-					for (IndexType j = begin_indexY; j < end_indexY; ++j)
-						for (IndexType i = begin_indexX; i < end_indexX; ++i)
-							function(i, j, k);
-				},
-				policy);
-}
-
-template<typename IndexType, typename Function>
-void parallelRangeFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, IndexType begin_indexZ, IndexType end_indexZ, const Function &function, ExecutionPolicy policy = ExecutionPolicy::Parallel)
-{
-	parallelRangeFor(begin_indexZ, end_indexZ,
-					 [&](IndexType kBegin, IndexType kEnd)
-					 {
-						 function(begin_indexX, end_indexX, begin_indexY, end_indexY, kBegin, kEnd);
-					 },
-					 policy);
-}
-}
+#include "impl/parallel_impl_tbb.h"
 #endif
 #endif //HINAPE_PARALLEL_H
