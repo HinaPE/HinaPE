3c1eb030aa02e07c6c7eaa46385baa598a4eeff9
Xayah-Hina <ycxiang14sep@gmail.com>
2023-01-31 16:45:19 +0800

    u: support tbb threading

--- physics/_experimental/test/test_parallel.h
+++ physics/_experimental/test/test_parallel.h
@@ -13,52 +13,46 @@
 
 TEST(TestParallel, Parallel)
 {
-	int scale = 10000;
+	int scale = 20000;
 	std::srand(std::time(nullptr));
 	std::vector<int> v1;
 	for (int i = 0; i < scale; ++i)
 		v1.push_back(std::rand());
 
 	auto f1 = std::async(std::launch::async,
-						 [&]()
+						 [&]() -> float
 						 {
 							 Timer timer;
 
-							 long long sum = 0;
 							 for (int i = 0; i < scale; ++i)
 							 {
 								 auto v2 = v1;
 								 std::sort(v2.begin(), v2.end());
-								 sum += v2[i];
 							 }
-							 std::cout << "sum = " << sum << std::endl;
 
 							 timer.duration("For");
+							 return timer._last_duration;
 						 });
 
 	auto f2 = std::async(std::launch::async,
-						 [&]()
+						 [&]() -> float
 						 {
 							 Timer timer;
 
-							 {
-								 long long sum = 0;
-								 Hina::Base::parallelFor(0, scale,
-														 [&](int i)
-														 {
-															 auto v2 = v1;
-															 std::sort(v2.begin(), v2.end());
-															 sum += v2[i];
-														 },
-														 Hina::Base::ExecutionPolicy::Parallel);
-								 std::cout << "sum = " << sum << std::endl;
-							 }
+							 Hina::Base::parallelFor(0, scale,
+													 [&](int i)
+													 {
+														 auto v2 = v1;
+														 std::sort(v2.begin(), v2.end());
+													 },
+													 Hina::Base::ExecutionPolicy::Parallel);
 
 							 timer.duration("parallelFor");
+							 return timer._last_duration;
 						 });
 
-	f1.get();
-	f2.get();
+	std::cout << "acceleration rate: " << f1.get() / f2.get() << std::endl;
+	std::cout << "thread num: " << std::thread::hardware_concurrency() << std::endl;
 }
 
 #endif //HINAPE_TEST_PARALLEL_H
