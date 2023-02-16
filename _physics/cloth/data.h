0216ac0d256f9cd2ba362b1fba3913d19ddd1bd8
Xayah-Hina <ycxiang14sep@gmail.com>
2023-02-16 11:18:47 +0800

    pragma

---
+++ _physics/cloth/data.h
@@ -0,0 +1,12 @@
+#ifndef HINAPE_CLOTH_DATA_H
+#define HINAPE_CLOTH_DATA_H
+
+namespace HinaPE
+{
+class PBDClothData
+{
+public:
+};
+}
+
+#endif //HINAPE_CLOTH_DATA_H
---
+++ _physics/cloth/solver.h
@@ -0,0 +1,12 @@
+#ifndef HINAPE_CLOTH_SOLVER_H
+#define HINAPE_CLOTH_SOLVER_H
+
+namespace HinaPE
+{
+class PBDClothSolver
+{
+public:
+};
+}
+
+#endif //HINAPE_CLOTH_SOLVER_H
