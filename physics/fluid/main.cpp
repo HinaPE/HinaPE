6986f8d1343ad1557e5d737f9c63d177f7d10b4f
Xayah-Hina <ycxiang14sep@gmail.com>
2023-02-17 21:35:42 +0800

    framework

--- physics/fluid/main.cpp
+++ physics/fluid/main.cpp
@@ -1,4 +1,19 @@
+#include "app.h"
+#include "lagrangian/solver.h"
+
+class FluidApp : public Kasumi::App
+{
+protected:
+	void prepare() final
+	{
+	}
+	void update(double dt) final
+	{
+	}
+};
+
 auto main() -> int
 {
+	std::make_shared<FluidApp>()->launch();
 	return 0;
 }
