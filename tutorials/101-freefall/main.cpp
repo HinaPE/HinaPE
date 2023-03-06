b1930d365cf3c3060613de2acec09e4de6d375aa
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-07 03:47:34 +0800

    save

--- tutorials/101-freefall/main.cpp
+++ tutorials/101-freefall/main.cpp
@@ -9,32 +9,22 @@ public:
 	void update(real dt) {}
 };
 
-//class FreeFall : public Kasumi::Renderer3D
-//{
-//protected:
-//	void init() final
-//	{
-//		sphere = std::make_shared<Kasumi::SphereObject>();
-//		sphere->POSE.position = {0, 5, 0};
-//		add_obj(sphere);
-//
-//		cube = std::make_shared<Kasumi::CubeObject>();
-//		cube->POSE.position = {0, 0, 0};
-//		add_obj(cube);
-//	}
-//	void step(real dt) final
-//	{
-//		auto a = mVector3::Zero();
-//		HinaPE::TimeIntegration::semi_implicit_euler(dt, 1, sphere->POSE.position, a, {0, -9.8, 0});
-//	}
-//
-//	Kasumi::SphereObjectPtr sphere;
-//	Kasumi::CubeObjectPtr cube;
-//};
-
 auto main() -> int
 {
-	std::make_shared<SimpleFreeFallSystem>();
-//	std::make_shared<FreeFall>()->launch();
+	auto free_fall = std::make_shared<SimpleFreeFallSystem>();
+	auto renderer = std::make_shared<Kasumi::Renderer3D>();
+	renderer->_init = [&](const Kasumi::Scene3DPtr &scene)
+	{
+		auto sphere = std::make_shared<Kasumi::SphereObject>();
+		sphere->POSE.position = {0, 5, 0};
+		scene->add(sphere);
+		auto cube = std::make_shared<Kasumi::CubeObject>();
+		cube->POSE.position = {0, 0, 0};
+		scene->add(cube);
+	};
+	renderer->_step = [&](real dt)
+	{
+	};
+	renderer->launch();
 	return 0;
 }
