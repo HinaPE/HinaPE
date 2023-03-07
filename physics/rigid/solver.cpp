23edf38d13ceba461a41b529a866b8270e895288
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-07 17:24:03 +0800

    scene debug inspector

--- physics/rigid/solver.cpp
+++ physics/rigid/solver.cpp
@@ -4,7 +4,7 @@
 static auto toR(const mVector3 &v) -> reactphysics3d::Vector3 { return reactphysics3d::Vector3{v.x(), v.y(), v.z()}; }
 static auto toR(const mQuaternion &q) -> reactphysics3d::Quaternion { return reactphysics3d::Quaternion{q.x(), q.y(), q.z(), q.w()}; }
 static auto toR(const Kasumi::Pose &p) -> reactphysics3d::Transform { return reactphysics3d::Transform{toR(p.position), toR(mQuaternion(p.euler))}; }
-static auto toR(const HinaPE::RigidType &r) -> reactphysics3d::BodyType { switch (r) { case HinaPE::RigidType::Static: return reactphysics3d::BodyType::STATIC; case HinaPE::RigidType::Dynamic: return reactphysics3d::BodyType::DYNAMIC; case HinaPE::RigidType::Kinematic: return reactphysics3d::BodyType::KINEMATIC; } }
+static auto toR(const HinaPE::RigidType &r) -> reactphysics3d::BodyType { switch (r) { case HinaPE::RigidType::Static: return reactphysics3d::BodyType::STATIC; case HinaPE::RigidType::Dynamic: return reactphysics3d::BodyType::DYNAMIC; case HinaPE::RigidType::Kinematic: return reactphysics3d::BodyType::KINEMATIC; default: return reactphysics3d::BodyType::DYNAMIC; } }
 static auto toM(const reactphysics3d::Vector3 &v) -> mVector3 { return mVector3{v.x, v.y, v.z}; }
 static auto toM(const reactphysics3d::Quaternion &q) -> mQuaternion { return mQuaternion{q.w, q.x, q.y, q.z}; }
 static auto toM(const reactphysics3d::Transform &t) -> Kasumi::Pose { return Kasumi::Pose{toM(t.getPosition()), toM(t.getOrientation()).euler(), {1, 1, 1}}; }
