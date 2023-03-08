00103b95bc64fc524deaa97575bfe0b66c9d25ee
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-09 00:26:47 +0800

    density monitor

--- physics/fluid-particles/main.cpp
+++ physics/fluid-particles/main.cpp
@@ -26,6 +26,19 @@ struct NeighborSearchVisualization : public Kasumi::ObjectLines3DInstanced
 	}
 };
 
+struct DensityMonitor : public Kasumi::INSPECTOR
+{
+	explicit DensityMonitor(HinaPE::SPHSolverDataPtr data) : _data(std::move(data)) {}
+	HinaPE::SPHSolverDataPtr _data;
+
+protected:
+	void INSPECT() final
+	{
+		if (_data->_inst_id >= 0 && _data->_inst_id < _data->_densities.size())
+			ImGui::Text("Inst: %d, Density: %.3f", _data->_inst_id, _data->_densities[_data->_inst_id]);
+	}
+};
+
 auto main() -> int
 {
 	// prepare solver
@@ -41,6 +54,7 @@ auto main() -> int
 	solver->_emitter = emitter;
 
 	auto vis = std::make_shared<NeighborSearchVisualization>(data);
+	auto des_vis = std::make_shared<DensityMonitor>(data);
 
 
 	// set up init & step & debugger
@@ -64,6 +78,7 @@ auto main() -> int
 
 
 	renderer->inspect(solver.get());
+	renderer->inspect(des_vis.get());
 
 	// launch renderer
 	renderer->launch();
