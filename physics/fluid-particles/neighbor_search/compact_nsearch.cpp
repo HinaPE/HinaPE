5c4daf759bd8318a75de35f16dbb2f289cdf5055
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-08 09:21:06 +0800

    nsearch

---
+++ physics/fluid-particles/neighbor_search/compact_nsearch.cpp
@@ -0,0 +1,1 @@
+#include "compact_nsearch.h"
---
+++ physics/fluid-particles/neighbor_search/compact_nsearch.h
@@ -0,0 +1,15 @@
+#ifndef HINAPE_COMPACT_NSEARCH_H
+#define HINAPE_COMPACT_NSEARCH_H
+
+// Copyright (c) 2023 Xayah Hina
+// MPL-2.0 license
+
+#include "common.h"
+#include "nsearch/CompactNSearch.h"
+
+namespace HinaPE
+{
+
+} //namespace HinaPE
+
+#endif //HINAPE_COMPACT_NSEARCH_H
