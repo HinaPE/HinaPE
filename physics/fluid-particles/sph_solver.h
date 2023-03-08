6ad40402f06eaa52a70b76eba5ffb20c75a9e729
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-09 00:51:13 +0800

    save

--- physics/fluid-particles/sph_solver.h
+++ physics/fluid-particles/sph_solver.h
@@ -69,7 +69,7 @@ public:
 	// sph
 	real target_density 	= 1000; // water density
 	real target_spacing 	= 0.1;
-	real kernel_radius_over_target_spacing = 1.8;
+	real kernel_radius_over_target_spacing = 11.8;
 	real kernel_radius 		= kernel_radius_over_target_spacing * target_spacing;
 
 	PointNeighborSearch3Ptr _neighbor_search;
