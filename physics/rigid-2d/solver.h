738694108520a0e3b96430e07d2da7e36a2a1c07
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-09 19:20:03 +0800

    support box2d rigid2d

---
+++ physics/rigid-2d/solver.h
@@ -0,0 +1,14 @@
+#ifndef HINAPE_RIGID2D_SOLVER_H
+#define HINAPE_RIGID2D_SOLVER_H
+
+// Copyright (c) 2023 Xayah Hina
+// MPL-2.0 license
+
+namespace HinaPE
+{
+class Rigid2DSolver
+{
+};
+}
+
+#endif //HINAPE_RIGID2D_SOLVER_H
