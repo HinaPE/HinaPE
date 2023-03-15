ab9b2c80063d447b94f997b0f8451786e55ffccc
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-15 22:25:30 +0800

    [fluid] setup hybrid fluid solver

---
+++ physics/fluid-hybrid/apic_solver.h
@@ -0,0 +1,14 @@
+#ifndef HINAPE_APIC_SOLVER_H
+#define HINAPE_APIC_SOLVER_H
+
+// Copyright (c) 2023 Xayah Hina
+// MPL-2.0 license
+
+namespace HinaPE
+{
+class APICSolver
+{
+};
+} // namespace HinaPE
+
+#endif //HINAPE_APIC_SOLVER_H
---
+++ physics/fluid-hybrid/flip_solver.cpp
@@ -0,0 +1,1 @@
+#include "flip_solver.h"
