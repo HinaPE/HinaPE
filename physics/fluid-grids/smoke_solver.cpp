78ff29fbbf198befb6c958f533c19e2e95be62c0
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-16 17:56:14 +0800

    [grid] update

--- physics/fluid-grids/smoke_solver.cpp
+++ physics/fluid-grids/smoke_solver.cpp
@@ -3,6 +3,8 @@
 HinaPE::SmokeSolver::Data::Data()
 {
 	_density = std::make_shared<Geom::VertexCenteredScalarGrid3>();
-	_density->resize({15, 15, 15}, mVector3::One(), mVector3::Zero(), 0);
+	_density->resize({50, 50, 50}, mVector3::One(), mVector3::Zero(), 0);
+	(*_density)(1,2,3) = 1;
+	(*_density)(11,22,33) = 1;
 	track(&(*_density));
 }
