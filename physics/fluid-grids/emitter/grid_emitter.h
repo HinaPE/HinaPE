c4ed8f6cf877e2d6419adc04ec265846b32ae04c
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-17 17:15:54 +0800

    [grid] update

--- physics/fluid-grids/emitter/grid_emitter.h
+++ physics/fluid-grids/emitter/grid_emitter.h
@@ -18,6 +18,18 @@ class VolumeGridEmitter3 : public GridEmitter3
 {
 public:
 	void emit() final;
+
+	Geom::ImplicitSurface3Ptr _source_region = std::make_shared<Geom::SurfaceToImplicit3>(std::make_shared<Geom::Sphere3>());
+	const std::function<real(real, const mVector3 &, real)> scalar_mapper = [](real sdf, const mVector3 &, real old)
+	{
+		const real grid_spacing = 0.1;
+		real step = Constant::One - Math::smeared_heaviside_sdf(sdf / grid_spacing);
+		return std::max(old, step); // (max - min) * step + min, max = 1 min = 0
+	};
+	const std::function<real(real, const mVector3 &, const mVector3 &)> vector_mapper = [](real x, const mVector3 &pt, const mVector3 &old)
+	{
+		return x;
+	};
 };
 
 using GridEmitter3Ptr = std::shared_ptr<GridEmitter3>;
