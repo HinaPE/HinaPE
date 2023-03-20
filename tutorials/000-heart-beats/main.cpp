faa2a275651575b09cdb1b588f601eb0c9fc3165
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-20 15:53:50 +0800

    [shader] build shaders into exe

--- tutorials/000-heart-beats/main.cpp
+++ tutorials/000-heart-beats/main.cpp
@@ -1,46 +1,17 @@
 #include "shader_painter/shader_painter.h"
 
-static std::string painter_fragment_src = R"(
-#version 330 core
-out vec4 FragColor;
-
-uniform vec2 iResolution;
-uniform float iTime;
-vec2 fragCoord = gl_FragCoord.xy;
-void main() {
-    vec2 p = (2.0 * fragCoord - iResolution.xy) / min(iResolution.x, iResolution.y);
-
-    // background color
-    vec3 bcol = vec3(1.0, 0.8, 0.7-0.07*p.y)*(1.0-0.25*length(p));
-
-    // animate
-    float tt = mod(iTime, 1.5) / 1.5;
-    float ss = pow(tt, 0.2) * 0.5 + 0.5;
-    ss = 1.0 + ss*0.5*sin(tt*6.2831*3.0 + p.y*0.5)*exp(-tt*4.0);
-    p *= vec2(0.5, 1.5) + ss*vec2(0.5, -0.5);
-
-    p.y -= 0.25;
-    float a = atan(p.x, p.y)/3.141593;
-    float r = length(p);
-    float h = abs(a);
-    float d = (13.0*h - 22.0*h*h + 10.0*h*h*h)/(6.0-5.0*h);
-
-    float s = 0.75 + 0.75*p.x;
-    s *= 1.0-0.4*r;
-    s = 0.3 + 0.7*s;
-    s *= 0.5+0.5*pow(1.0-clamp(r/d, 0.0, 1.0), 0.1);
-    vec3 hcol = vec3(1.0, 0.4*r, 0.3)*s;
-
-    vec3 col = mix(bcol, hcol, smoothstep(-0.01, 0.01, d-r));
-
-    FragColor = vec4(col, 1.0);
-}
-)";
+#include <cmrc/cmrc.hpp>
+CMRC_DECLARE(HeartBeats);
 
 auto main() -> int
 {
+	auto fs = cmrc::HeartBeats::get_filesystem();
+	auto heart = fs.open("shaders/heart.glsl").begin();
+
 	auto painter = std::make_shared<Kasumi::ShaderPainter>();
-	painter->load_shader(painter_fragment_src.c_str());
+	painter->close_menu();
+	painter->clean_mode();
+	painter->load_shader(heart);
 	painter->launch();
 	return 0;
 }
