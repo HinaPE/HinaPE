0f250eb6233c21bf812e1060d034e535153542f8
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-20 17:02:35 +0800

    [paper impl] clouds - doba2020

---
+++ tutorials/401-impl-cloud-doba2000/main.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Xayah Hina
+// MPL-2.0 license
+// Implementation of "A simple, efficient method for realistic animation of clouds" by Dobashi, Yoshinori & Kaneda, Kazufumi & Yamashita, Hideo & Okita, Tsuyoshi & Nishita, Tomoyuki.
+// https://dl.acm.org/doi/pdf/10.1145/344779.344795
+
+auto main() -> int
+{
+	return 0;
+}
