7b59a56ea0fe023249305243d2198638b5753499
Xayah-Hina <ycxiang14sep@gmail.com>
2023-05-09 16:10:45 +0800

    [scene] support menu open / export / save scene

--- tutorials/006-triangle-mesh-geometry/main.cpp
+++ tutorials/006-triangle-mesh-geometry/main.cpp
@@ -5,9 +5,10 @@ auto main() -> int
 	Kasumi::Renderer3D::DEFAULT_RENDERER._init = [&](const Kasumi::Scene3DPtr &scene)
 	{
 		auto bunny = std::make_shared<Kasumi::BunnyObject>();
+		bunny->set_color(HinaPE::Color::WHITE);
 		scene->add(bunny);
 	};
-	Kasumi::Renderer3D::DEFAULT_RENDERER.close_inspector();
+	Kasumi::Renderer3D::DEFAULT_RENDERER.close_benchmark();
 	Kasumi::Renderer3D::DEFAULT_RENDERER.launch();
 	return 0;
 }
