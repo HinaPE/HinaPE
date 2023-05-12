edf517f3dfd559765f917c65a78298e44d997d10
Xayah-Hina <ycxiang14sep@gmail.com>
2023-05-13 00:52:15 +0800

    [save] error voxelize

--- tutorials/006-triangle-mesh-geometry/main.cpp
+++ tutorials/006-triangle-mesh-geometry/main.cpp
@@ -1,14 +1,42 @@
 #include "renderer3D/renderer3D.h"
+class Data : public Kasumi::ObjectGrid3D
+{
+public:
+	Data()
+	{
+		auto bunny = std::make_shared<Kasumi::BunnyObject>();
+		_data = bunny->voxelize();
+		track(&_data);
+	}
+
+private:
+	HinaPE::Geom::DataGrid3<int> _data;
+};
+
+class BoxDomain : public Kasumi::CubeObject
+{
+public:
+	BoxDomain()
+	{
+		NAME = "Domain";
+		_switch_surface();
+		_switch_bbox();
+		flip_normal(); // for inner collision
+		set_color(HinaPE::Color::WHITE);
+	}
+};
 
 auto main() -> int
 {
 	Kasumi::Renderer3D::DEFAULT_RENDERER._init = [&](const Kasumi::Scene3DPtr &scene)
 	{
-		auto bunny = std::make_shared<Kasumi::BunnyObject>();
-		bunny->set_color(HinaPE::Color::WHITE);
-		scene->add(bunny);
+		auto domain = std::make_shared<BoxDomain>();
+		auto data = std::make_shared<Data>();
+		scene->add(data);
+//		scene->add(domain);
 	};
 	Kasumi::Renderer3D::DEFAULT_RENDERER.close_benchmark();
+	Kasumi::Renderer3D::DEFAULT_RENDERER.dark_mode();
 	Kasumi::Renderer3D::DEFAULT_RENDERER.launch();
 	return 0;
 }
