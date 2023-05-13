b19c2524b5f6593d40487cc16490b52828eef12c
Xayah-Hina <ycxiang14sep@gmail.com>
2023-05-13 19:10:39 +0800

    [surface particles] almost success

--- tutorials/006-triangle-mesh-geometry/main.cpp
+++ tutorials/006-triangle-mesh-geometry/main.cpp
@@ -7,8 +7,8 @@ class Data : public Kasumi::ObjectGrid3D
 public:
 	Data()
 	{
-		auto bunny = std::make_shared<Kasumi::BunnyObject>();
-		_data = bunny->voxelize();
+		bunny = std::make_shared<Kasumi::BunnyObject>();
+		auto res = bunny->generate_surface();
 		track(&_data);
 	}
 
@@ -29,15 +29,26 @@ public:
 	}
 };
 
+struct Fluid : public Kasumi::ObjectParticles3D
+{
+	Fluid()
+	{
+		positions = std::make_shared<Kasumi::BunnyObject>()->generate_surface();
+		track(&positions);
+	}
+	std::vector<mVector3> positions;
+};
+
 auto main() -> int
 {
+	auto domain = std::make_shared<BoxDomain>();
+	auto fluid = std::make_shared<Fluid>();
+
 	Kasumi::Renderer3D::DEFAULT_RENDERER._init = [&](const Kasumi::Scene3DPtr &scene)
 	{
-		auto domain = std::make_shared<BoxDomain>();
-		auto data = std::make_shared<Data>();
-		scene->add(data);
-		//		scene->add(data->bunny);
+		scene->add(fluid);
 		scene->add(domain);
+		scene->_scene_opt._particle_mode = true;
 	};
 	Kasumi::Renderer3D::DEFAULT_RENDERER.close_benchmark();
 	Kasumi::Renderer3D::DEFAULT_RENDERER.dark_mode();
